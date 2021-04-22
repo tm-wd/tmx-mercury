465fadf08fc59fe1ba4b7e621b972a551b7e9bf1
Thiago Melo <thiago.tmwd@gmail.com>
2021-04-21 21:02:09 -0300

    Adjusts layout

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
+import ScrollReveal from 'scrollreveal'
+
 function Cards({nome, sigla, regiao, link}){
+    
+    useEffect(() => {
+        ScrollReveal().reveal('.cards');
+    }, [])
+
     return(
-        <div className="col-lg-4 mb-3">
+        <div className="col-lg-4 mb-3 cards">
             <div className="card ">
                 <div className="card-header"><strong>{nome}</strong> | <span className="text-primary">{sigla}</span></div>
                 <div className="card-body text-secondary">

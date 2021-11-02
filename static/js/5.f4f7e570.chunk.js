c736d5fc1078c2cb664a0a04f183ae9ee60cc486
Thiago Melo <thiago.tmwd@gmail.com>
2021-11-02 09:36:54 -0300

    Updates

--- static/js/5.f4f7e570.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonptmx-mercury"]=this["webpackJsonptmx-mercury"]||[]).push([[5],{64:function(e,t,r){"use strict";r.r(t);var o=r(1);r(0);t.default=function(e){var t=e.search;return Object(o.jsx)("input",{type:"text",onKeyUp:t,className:"form-control form-control-lg mb-4",placeholder:"Digite o nome de um estado aqui..."})}}}]);
-//# sourceMappingURL=5.f4f7e570.chunk.js.map
